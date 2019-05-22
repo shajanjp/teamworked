73c590934177808ceabe9e8d18dbef5eb6484f41
Shajan Jacob <shajanjp@gmail.com>
2019-05-22 17:23:03 +0530

    fixes API and flow logic

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let currentProjectId;
 
 $('#tasklist select').dropdown({onChange: updateCurrentTask});
 $('#boardlist select').dropdown({onChange: updateTaskList})
+$('.ui .accordion').accordion()
 
 function updateCurrentTask(value, text){
   currentTaskId = value;
@@ -48,63 +49,73 @@ function updateBoardList(value, text){
   })
 }
 
-getProjects()
-.then(res => {
-  let projectListMarkup = '';
-  res.projects.forEach(project => {
-    projectListMarkup += `<option value="${project.id}">${project.name}</option>`;
-  })
-  $('#projectlist select').html(projectListMarkup);
-  $('#projectlist select').dropdown({
-    onChange: updateBoardList
-  });
-})
-
-
-function getCurrentDateTime() {
-  let d = new Date(),
-  month = '' + (d.getMonth() + 1),
-  day = '' + d.getDate(),
-  year = d.getFullYear();
-  hours = d.getHours();
-  minutes = d.getMinutes();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-  if (hours.length < 2) hours = '0' + hours;
-  if (minutes.length < 2) minutes = '0' + minutes;
-
-  return {
-    date: [year, month, day].join(''), 
-    time: [hours, minutes].join(':')
-  };
+function secondsToTime(seconds){
+  let h = (seconds/3600).toFixed(0);
+  let m = ((seconds%3600)/60).toFixed(0);
+  let s = (seconds%60).toFixed(0);
+  return `${h}:${m}:${s}`;
 }
 
+getTimers()
+.then(timersRes => {
+  let timersMarkup = '';
+  console.log('timers', timersRes);
+  timersRes.timers.forEach(timer => {
+    timersMarkup += `<div class="ui segment" data-timerId='${timer.id}' data-isRunning='${(timer.running==true) ? '1' : '0'}'>
+    <h4 class="ui header">${timer.projectName}</h4>
+    <p>${timer.taskName}</p>
+    <div class="ui icon basic fluid buttons">
+    <div class="ui button" title="Start / Pause / Resume timer" data-state="paused" id="toggle-timer">
+    <i class="${(timer.running==true) ? 'orange pause' : 'green play'} icon"></i>
+    </div>
 
-let intervalCounterInstance;
-let logCounter;
+    <div class="ui button" id="timer-time">
+    ${secondsToTime(timer.duration)}
+    </div>
+
+    <div class="ui button" title="Stop timer" id="complete-timer">
+    <i class="square red play icon"></i>
+    </div>
+
+    <div class="ui button" title="Save timelog" id="save-timer">
+    <i class="blue save icon"></i>
+    </div>
+    </div>
+    </div>`
+  })
+
+  $('#timers-list').html(timersMarkup);
+
+})
+
+$('.create-timer').on('click', (e) => {
+  $('.create-timer-form').modal('show');
+  getProjects()
+  .then(res => {
+    let projectListMarkup = '';
+    res.projects.forEach(project => {
+      projectListMarkup += `<option value="${project.id}">${project.name}</option>`;
+    })
+    $('#projectlist select').html(projectListMarkup);
+    $('#projectlist select').dropdown({
+      onChange: updateBoardList
+    });
+  })
+})
 
 $('#toggle-timer').on('click', (e) => {
   // start
   if(timerIsRunning == false){
-    logCounter = 0;
     timerIsRunning = true;
     $('#toggle-timer i').removeClass('play green');
     $('#toggle-timer i').addClass('pause orange');
-    intervalCounterInstance = setInterval(function(){
-      logCounter += 1; 
-      timeCounterElement.html(`${(logCounter / 60).toFixed(0)}:${logCounter % 60}`)
-    }, 1000);
   }
 
   // pause
   else {
-    let currentDateTime = getCurrentDateTime();
     timerIsRunning = false;
     $('#toggle-timer i').removeClass('pause orange');
     $('#toggle-timer i').addClass('play green');
-    clearInterval(intervalCounterInstance);
-    timeCounterElement.html('00:00');
   }
 
 })
